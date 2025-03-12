bf8e5433c2535b2c467336c375b8ad0ecc5b22b5
ZevDev54 <zevy.johnston@gmail.com>
2025-03-11 20:45:10 -0600

    Complete FamilyTree assignment

--- src/FamilyTree/FamilyTreeMain.java
+++ src/FamilyTree/FamilyTreeMain.java
@@ -1,5 +1,7 @@
 package FamilyTree;
 
+import Utilities.Library;
+
 public class FamilyTreeMain {
 
     public static void run() {
@@ -38,26 +40,88 @@ public class FamilyTreeMain {
 
 
         int count = countFamily(person1);
-        System.out.println(count);
+        System.out.println("Whole family size: "+count);
+        printFamily(person1, 1);
+        System.out.println();
+
+        printCanadians(person1, 1);
+
+//        Person locate = treeSearch(person1, "Zev");
+//        System.out.println(locate.name);
+        System.out.println();
+        System.out.println("Print family branch from person. Enter person name...");
+        String inputName = Library.input.nextLine();
+        printFromHere(person1, inputName);
     }
 
     public static int countFamily(Person ptemp) {
-        int num = 0;
-
-
-
-        for (int i = 0; i < ptemp.children.size(); i++) {
-            countFamily(ptemp.children.get(i));
+        if(ptemp.children.size() > 0) {
+            int num = 0;
+            for (int i = 0; i < ptemp.children.size(); i++) {
+                num += countFamily(ptemp.children.get(i));
+            }
+            return num + 1;
         }
-        if(ptemp.children.size() > 0)
-            return ptemp.children.size() + countFamily(ptemp);
-        else
+        else {
             return 1;
-    }
-
-    public static void printCanadians(Person ptemp) {
-
+        }
     }
 
 
+
+    public static void printFamily(Person ptemp, int depth) {
+        if(depth == 1){
+            System.out.println(ptemp.name+" (root)");
+        }
+        if(ptemp.children.size() > 0) {
+            for (int i = 0; i < ptemp.children.size(); i++) {
+                String spacing = "";
+                for (int d = 0; d < depth; d++) spacing+="-";
+                System.out.println(spacing+ptemp.children.get(i).name);
+                printFamily(ptemp.children.get(i), depth+1);
+            }
+        }
+    }
+
+
+    public static void printCanadians(Person ptemp, int depth) {
+        if(depth == 1){
+            System.out.println("Print Canadians only");
+        }
+        if(ptemp.children.size() > 0) {
+            for (int i = 0; i < ptemp.children.size(); i++) {
+                if(ptemp.children.get(i).country.equalsIgnoreCase("Canada")) {
+                    String spacing = "";
+                    for (int d = 0; d < depth; d++) spacing += "-";
+                    System.out.println(spacing + ptemp.children.get(i).name);
+                }
+
+                printCanadians(ptemp.children.get(i), depth+1);
+            }
+        }
+    }
+
+    public static void printFromHere(Person root, String searchName){
+        Person newRoot = treeSearch(root, searchName);
+        if(newRoot == null) {
+            System.out.println("Invalid name! ("+searchName+")");
+            return;
+        } //invalid name case
+        printFamily(newRoot, 1);
+    }
+
+    public static Person treeSearch(Person ptemp, String matchName){
+        Person res = null;
+
+        if(ptemp.name.equalsIgnoreCase(matchName)){
+            return ptemp;
+        }
+        if(ptemp.children.size() > 0) {
+            //  this is cool! ---V
+            for (int i = 0; res == null && i < ptemp.children.size(); i++) {
+                res = treeSearch(ptemp.children.get(i), matchName);
+            }
+        }
+        return res;
+    }
 }
